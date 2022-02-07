0e4c1f3c37230ce2cb86ac62b9add1d639bca34f
anabramos <anaramosbarretto@gmail.com>
2022-02-07 20:08:39 +0000

    final commit

--- static/js/locations.js
+++ static/js/locations.js
@@ -2,6 +2,7 @@ const nature = document.getElementById("nature");
 const culture = document.getElementById("culture");
 const hiddenGems = document.getElementById("hidden-gems");
 const lekkers = document.getElementById("lekkers");
+const drinks = document.getElementById("drinks");
 
 nature.onclick = function(){
     initMap(natureQueries)
@@ -19,6 +20,10 @@ lekkers.onclick = function(){
     initMap(lekkersQueries)
 }
 
+drinks.onclick = function(){
+    initMap(drinksQueries)
+}
+
 const natureQueries = [
     {
         label: 'Castricum Beach',
@@ -27,10 +32,22 @@ const natureQueries = [
         description: "A short train ride from Amsterdam, you exit at train station Castricum. From here it is a lovely (1 hour) walk through the forest and dunes to the beach. It is much quieter than some other beaches near Amsterdam, and during your walk you will encounter lovely highland cows.(Heleen) I like Castricum aan Zee, because it has a beautiful beach and it’s a place where I can clear my head while listening to the waves.(Dilara)"
     },
     {
+        label: 'Club Zand',
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Dilara',
+        description: "My favorite spot for winter beach walks is Castricum aan Zee. During the lockdown they had a very nice bar at the beach with take away food and drinks."
+    },
+    {
         label: 'De Alde Feanen National Park',
         fields: ['name', 'formatted_address', 'geometry'],
         contributors: 'Hélène',
         description: "Beautiful for a boating day or cycling/walking around."
+    },
+    {
+        label: 'Einkhuizen',
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Tessa',
+        description: "Enkhuizen in Noord-Holland, the town as a whole, the VVV wandeling and the Zuiderzeemuseum which is an amazing insight into historical daily life in the region – its touristy so pick your day wisely, it’ll be worth it!"
     },
     {
         label: 'Elswout',
@@ -43,10 +60,16 @@ const natureQueries = [
         fields: ['name', 'formatted_address', 'geometry'],
         contributors: 'Ginger',
         description: "The Corversbos, the woods near my house. During Covid lockdowns & isolation I've been able to walk here nearly every day. The beauty of the trees has been a wonderful consolation and mood-lifter. Occasionally I even see small deer."
+    },
+    {
+        label: 'The Kennermerduinen',
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Tessa',
+        description: "You can do a station to station walk here that shows some of the more beautiful apsects of north holland, it takes you through woods, dunes, the beach and back round again, ending at a very cute little station with a traditional station cafe where you can take a load off and have a - after 16km very well deserved - beer and snack."
     }
-  ];
+];
 
-  const cultureQueries = [
+const cultureQueries = [
     {
         label: 'De Pont Museum of Contemporary Art',
         fields: ['name', 'formatted_address', 'geometry'],
@@ -107,15 +130,9 @@ const natureQueries = [
         contributors: 'Victoria',
         description: "Batavierhuis is an art residency for musicians based in Rotterdam. Every single week you have the chance of seeing fantastic musicians performing at this charming house for a fair price. The music is not the only good thing. As the place is run by the artists themselves, the atmosphere is super light and friendly. I had the chance of meeting amazing people there and, as a music lover yourself, I am sure you would follow the same path :). Besides that, Batavierhuis is super close to my own house! So, it is two programmes in one as you are more than welcome to come for a good Brazilian coffee."
     }
-  ];
+];
 
-  const hiddenGemsQueries = [
-    {
-        label: 'Begijnhofkapel',
-        fields: ['name', 'formatted_address', 'geometry'],
-        contributors: 'Bertan',
-        description: "Probably one of my favorite places to take tourists. This is a small oasis in the middle of Amsterdam. It used to be a convict for Nuns. To this day only women are permitted to live there. There is a really cute church and chapel in the middle. The place hosts one of the oldest standing houses in Amsterdam. One of the first places I saw when I moved to Amsterdam which just felt so magical and special. Really gives you the quintessential Amsterdam vibe."
-    },
+const hiddenGemsQueries = [
     {
         label: 'Pension Homeland',
         fields: ['name', 'formatted_address', 'geometry'],
@@ -153,26 +170,37 @@ const natureQueries = [
         description: "Come and see another city for a change and go to the Hague, visit Panorama Mesdag. Give me a call, and I give you both a tour and explanation about this very special, huge painting. It takes only 30 minutes and a cup coffee (or something else). After that you can go to the Kunstmuseum or go via Noordwijk beach (for drinks and dinner with sunset) back home."
     },
     {
-        label: 'Einkhuizen',
-        fields: ['name', 'formatted_address', 'geometry'],
-        contributors: 'Tessa',
-        description: "Enkhuizen in Noord-Holland, the town as a whole, the VVV wandeling and the Zuiderzeemuseum which is an amazing insight into historical daily life in the region – its touristy so pick your day wisely, it’ll be worth it!"
-    },
-    {
         label: 'IJ Hallen',
         fields: ['name', 'formatted_address', 'geometry'],
         contributors: 'Ana',
-        description: "Once per month, during a weekend, the NDSM Wharf is taken by the biggest fleamarket of Europe. Filled with collector items, furtnitures, clothing, it is a great place to go shopping or just for a weekend visit. Bring your haggle abilities and you may end up scoring some goods for little monies."
+        description: "Once per month, during a weekend, the NDSM Wharf is taken by the biggest fleamarket of Europe. Filled with collector items, furtnitures, clothing, it is a great place to go shopping or just for a morning visit. Bring your haggle abilities and you may end up scoring some goods for little monies."
     },
+    {
+        label: 'Begijnhofkapel',
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Bertan',
+        description: "Probably one of my favorite places to take tourists. This is a small oasis in the middle of Amsterdam. It used to be a convict for Nuns. To this day only women are permitted to live there. There is a really cute church and chapel in the middle. The place hosts one of the oldest standing houses in Amsterdam. One of the first places I saw when I moved to Amsterdam which just felt so magical and special. Really gives you the quintessential Amsterdam vibe."
+    },
+    {
+        label: 'The Oba - De Hallen ',
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Nimalka',
+        description: "Their chocolat au pain are delicious, as is their tuna toastie (carry tobacco with you), and on Sundays they do pancakes which are also really good! I used to go an write my thesis there. There’s also the food hall nearby. Whcich is kind of a bougie version of what foodtruck culture is supposed to be. More importantly, there is a cinema next door. So to make myself stay at the café and work the whole day without leaving to go take a nap (that always turned into a full sleep time) I would buy a ticket for an evening show and that would be my reward at the end of the work day. Also Ana will be able to give you the lowdown on subscriptions for the cinemas and also the different cinemas that show different types of films!!!."
+    },
+];
 
-  ];
-
-  const lekkersQueries = [
+const lekkersQueries = [
     {
         label: 'Café Public Space',
         fields: ['name', 'formatted_address', 'geometry'],
         contributors: 'Laura Urbo',
         description: "It's very Nordic in its design and modern in its approach to food. Chill vibes, good coffee and a nice location to visit after Eye Museum or taking a walk along the river."
+    },
+    {
+        label: 'Starbucks Reserve: The Bank',
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Nimalka',
+        description: "this probably seems like a very strange suggestion but the logic is actually quite brilliant (if I do say so myself). The Dutch (bless their hearts) and especially the office colleagues are very judgy and snooty about Starbucks so for the most part you don’t find them in there. Instead you find more North American people inside and you hear a lot more English white noise inside. I used to use Starbucks as my escape when the Dutch became too Dutch and when the office became too stifling. I found the inside atmosphere of Starbucks would calm me down enough and made me feel more at home. It was the closes culture to what I had grown up with in Dubai. I’d go in sometimes have a nice cry with my coffee and then come back to the office. I always knew id never ever run into any office colleagues in there. It was my safe space."
     },
     {
         label: 'Vuurtoreneiland',
@@ -181,29 +209,11 @@ const natureQueries = [
         description: "This is one of the best places to go with Terrance when you get here. It is a restaurant on a very small island off Amsterdam. You get there by boat. On the boar ride there they serve you cava and bites. And when you get there the food is amazing, especially the wine. My friends took me here the first time and I loved it. Then I took my boyfriend when we started dating, and might be a big reason why he loves me to this day."
     },
     {
-        label: 'Wynand Fockink Proeflokaal and Spirits',
-        fields: ['name', 'formatted_address', 'geometry'],
-        contributors: 'Bertan',
-        description: "This is a hidden gem of Amsterdam. It’s the ultimate experience in the center of the city, but totally unknown and invisible to the hordes of tourists. A historic 17th-century liquor store & tasting room selling liqueur & Jenever in an old world setting. I am a very regular visitor in the summers. Especially on weekend afternoons, or on a Friday after work."
-    },
-    {
-        label: 'Club Zand',
-        fields: ['name', 'formatted_address', 'geometry'],
-        contributors: 'Dilara',
-        description: "My favorite spot for winter beach walks is Castricum aan Zee. During the lockdown they had a very nice bar at the beach with take away food and drinks."
-    },
-    {
         label: 'Haagse Markt',
         fields: ['name', 'formatted_address', 'geometry'],
         contributors: 'Mette',
         description: "Biggest food market with sooo many goodies and very affordable fruit and veg from all over the world."
-    },
-    {
-        label: 'Pelgrim Rotterdam',
-        fields: ['name', 'formatted_address', 'geometry'],
-        contributors: 'Victoria',
-        description: "Pelgrim is a traditional brewery at the heart of Delsfhaven - the oldest neighbourhood in Rotterdam. The atmosphere and the beers are great! If it is not raining or too cold, I would highly recommend sitting outside by the canal."
-    },    
+    },   
     {
         label: 'Café Noorderlicht',
         fields: ['name', 'formatted_address', 'geometry'],
@@ -222,6 +232,68 @@ const natureQueries = [
         contributors: 'Tessa & Sanne',
         description: "The Saturday market at the Noordermarkt is a Jordaan staple, you can buy anything from flowers to fruits, fish, cheeses and loads of old bits and bobs. You'll find us here almost every week, having coffee and croissants at Kat in de Wijngaert - so feel free to join us there some time!"
     },
-    
-  ];
+    {
+        label: "The Breakfastclub Sloterdijk",
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Nimalka',
+        description: "This place has one of my favourite things of ALL time – chicken and waffles. I am a lover of fried chicken, it is my comfort food especially when it comes to a broken heart. Everyone in the office has many (silly) opinions on chicken and waffles for people who have never had any. The one I had here was the closest I found to my favourite that I used to eat in Canada! Their other food is also good as is their bloody mary’s."
+    },
+    {
+        label: "Saravanaa Bhavan Amsterdam",
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Nimalka',
+        description: "This place is a vegetarian indian restaurant that even though overpriced is exactly like the kind of food I will find in india. You will also see a lot of the indian diaspora in here and will here a lot of indian language background noise! South africa has a big indian-origin population and so maybe this is something that would like as well. Their south indian coffee is yummy as is their chai (chai tea.  is NOT a thing….pass it on). The atmosphere inside is also completely indian and can work as an escape from the Dutch as well."
+    },
 
+];
+
+const drinksQueries = [
+    {
+        label: 'Wynand Fockink Proeflokaal and Spirits',
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Bertan',
+        description: "This is a hidden gem of Amsterdam. It’s the ultimate experience in the center of the city, but totally unknown and invisible to the hordes of tourists. A historic 17th-century liquor store & tasting room selling liqueur & Jenever in an old world setting. I am a very regular visitor in the summers. Especially on weekend afternoons, or on a Friday after work."
+    },
+    {
+        label: 'Stadsbrouwerij De Pelgrim',
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Victoria',
+        description: "Pelgrim is a traditional brewery at the heart of Delsfhaven - the oldest neighbourhood in Rotterdam. The atmosphere and the beers are great! If it is not raining or too cold, I would highly recommend sitting outside by the canal."
+    },
+    {
+        label: "No Man's Art Gallery & de bar",
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Ana',
+        description: "Everyone at the office knows No Man's. At a perfect distance from the office, in a 15min walk you will find yourself in a super cool gallery/bar & restaurant where even the cocktails are artworks, and the menu is beautifully curated. If you are lucky they might even have the best caramel pie you you will ever taste in your life."
+    },
+    {
+        label: "Café het Doktertje",
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Sanne',
+        description: "One of the oldest bars in Amsterdam, and very typical – an experience if you want one."
+    },
+    {
+        label: "Glouglou",
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Laura A',
+        description: "A lovely natural wine bar near a beautiful park"
+    },
+    {
+        label: "Cafe Slijterij Oosterling",
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Mechtild',
+        description: "A typical Dutch Brown Café: Comfortable, vintage atmosphere and great beer!"
+    },
+    {
+        label: "Brasserie Beinum",
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Kitty',
+        description: "Nice big terrace in the centre of town, good vibes!"
+    },
+    {
+        label: "Brouwerij 't IJ",
+        fields: ['name', 'formatted_address', 'geometry'],
+        contributors: 'Ana',
+        description: "Amsterdam based brewery where you can enjoy beers inside a windmill pretty much in the center of the city. And the best of all: They close at 8pm so you can still go to sleep on time - or cross the street to the bar next door like everyone does."
+    },
+]
